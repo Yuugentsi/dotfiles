6e871f8c37e5e71787c344521da6f04cc2131a3c
Yuugentsi <yuugentsi@users.noreply.github.com>
2026-06-07 04:19:35 -0300

    update

--- .config/hypr/shaders/grayscale.glsl
+++ .config/hypr/shaders/grayscale.glsl
@@ -10,9 +10,9 @@ void main() {
     vec4  c    = texture(tex, v_texcoord);
     float luma = dot(c.rgb, vec3(0.2126, 0.7152, 0.0722));
 
-    // warm
-    vec3  saturated = mix(vec3(luma), c.rgb, 1.6);
-    vec3  yellow    = vec3(1.0, 0.90, 0.6);
-    vec3  tinted    = saturated * yellow;
-    fragColor       = vec4(tinted * 0.62, c.a);
+    // sepia
+    vec3  saturated = mix(vec3(luma), c.rgb, 1.0);
+    vec3  sepia    = vec3(1.0, 0.75, 0.45);
+    vec3  tinted   = saturated * sepia;
+    fragColor      = vec4(tinted * 0.78, c.a);
 }
